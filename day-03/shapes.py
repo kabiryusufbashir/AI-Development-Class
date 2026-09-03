7896ed92a5cf7e2099e774d1c6ce3b288178eeb8
Piccolo <kabiryusufbashir@gmail.com>
2026-09-03 14:41:16 +0100

    Day 3 - Lession 2: Dimensions & Shapes

---
+++ day-03/shapes.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+vector = np.array([
+    10, 20, 30, 40, 50
+])
+
+matrix = np.array([
+    [23, 90, 89, 45],
+    [63, 20, 29, 35],
+    [53, 20, 239, 335],
+])
+
+print(matrix.ndim)
