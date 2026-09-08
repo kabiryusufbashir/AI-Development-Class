ed651b927454a610da7cd30f18fa24ed7d9c69da
Piccolo <kabiryusufbashir@gmail.com>
2026-09-08 13:48:23 +0100

    Day 4 - Lesson 6: Duplicates

---
+++ day-04/duplicates.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from pathlib import Path
+
+# __file__ means "this current Python file".
+# parent means "the folder that contains this file", which is day-04.
+# This makes Python look for:
+# day-04/data/transactions.csv
+file_path = Path(__file__).parent / "data" / "transactions.csv"
+
+df = pd.read_csv(file_path)
+
+print(df)
+print(df.duplicated().sum)
+print(df.drop_duplicates())
+
+clean_df = df.drop_duplicates(
+    subset=["transaction_id"]
+)
+
+print(clean_df)
+
