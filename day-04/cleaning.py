1131d5755a4f628de56d6950cea6714c90ec59dc
Piccolo <kabiryusufbashir@gmail.com>
2026-09-08 13:37:46 +0100

    Day 4 - Lesson 5: Missing Values

---
+++ day-04/cleaning.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from pathlib import Path
+
+# __file__ means "this current Python file".
+# parent means "the folder that contains this file", which is day-04.
+# This makes Python look for:
+# day-04/data/transactions.csv
+file_path = Path(__file__).parent / "data" / "transactions.csv"
+
+df = pd.read_csv(file_path)
+
+print(df.isnull().sum())
+
+clean_df = df.dropna(
+    subset = ["amount"]
+)
+
+print(clean_df)
+print(df)
